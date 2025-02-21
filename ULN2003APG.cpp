50ef8ee6d057da4d9e7f343625bb47db8cbc4599
優之心 谷 <yunoshin.tani.0819@gmail.com>
2025-02-21 21:35:24 +0900

    add:base

--- ULN2003APG.cpp
+++ ULN2003APG.cpp
@@ -1,1 +1,32 @@
 #include "ULN2003APG.h"
+#include <mbed.h>
+
+ULN2003APG::ULN2003APG(PinName IN1, PinName IN2, PinName IN3, PinName IN4) : _IN1(IN1), _IN2(IN2), _IN3(IN3), _IN4(IN4), _step_in{_IN1, _IN2, _IN3, _IN4}
+{
+    _IN1.write(0);
+    _IN2.write(0);
+    _IN3.write(0);
+    _IN4.write(0);
+}
+
+int ULN2003APG::hz_to_ms(int frequency)
+{
+    return (int)(1000.0 / frequency);
+}
+
+void ULN2003APG::one_phase(int pulse_frequency)
+{
+    int v = 1000.0 / pulse_frequency;
+
+    _IN1 = 1;
+    ThisThread::sleep_for(chrono::milliseconds(v));
+    _IN1 = 0;
+
+    _IN2 = 1;
+    ThisThread::sleep_for(chrono::milliseconds(v));
+    _IN2 = 0;
+
+    _IN3 = 1;
+    ThisThread::sleep_for(chrono::milliseconds(v));
+    _IN3 = 0;
+}
--- ULN2003APG.h
+++ ULN2003APG.h
@@ -3,4 +3,26 @@
 
 #include <mbed.h>
 
+class ULN2003APG
+{
+public:
+    ULN2003APG(PinName IN1, PinName IN2, PinName IN3, PinName IN4);
+
+    void one_phase(int pulse_frequency);
+    
+    void two_phase(int pulse_frequency);
+    
+    void half_step(int pulse_frequency);
+
+private:
+    DigitalOut _IN1;
+    DigitalOut _IN2;
+    DigitalOut _IN3;
+    DigitalOut _IN4;
+
+    DigitalOut _step_in[4];
+
+    int hz_to_ms(int frequency);
+};
+
 #endif // ULN2003APG_H
