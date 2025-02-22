9e12bd26a700800aeef442b956faa808e10b81aa
Yunoshin Tani <taniyunoshin@gmail.com>
2025-02-22 19:32:17 +0900

    create: .h .cpp

--- ULN2003APG.cpp
+++ ULN2003APG.cpp
@@ -1,22 +1,23 @@
 #include "ULN2003APG.h"
 #include <mbed.h>
 
-ULN2003APG::ULN2003APG(PinName IN1, PinName IN2, PinName IN3, PinName IN4) : _IN1(IN1), _IN2(IN2), _IN3(IN3), _IN4(IN4), _step_in{_IN1, _IN2, _IN3, _IN4}
+// public
+ULN2003APG::ULN2003APG(PinName IN1, PinName IN2, PinName IN3, PinName IN4) : _IN1(IN1), _IN2(IN2), _IN3(IN3), _IN4(IN4)
 {
-    _IN1.write(0);
-    _IN2.write(0);
-    _IN3.write(0);
-    _IN4.write(0);
+    step_stop();
 }
 
-int ULN2003APG::hz_to_ms(int frequency)
+void ULN2003APG::step_stop()
 {
-    return (int)(1000.0 / frequency);
+    _IN1 = 0;
+    _IN2 = 0;
+    _IN3 = 0;
+    _IN4 = 0;
 }
 
 void ULN2003APG::one_phase(int pulse_frequency)
 {
-    int v = 1000.0 / pulse_frequency;
+    int v = hz_to_ms(pulse_frequency);
 
     _IN1 = 1;
     ThisThread::sleep_for(chrono::milliseconds(v));
@@ -29,4 +30,88 @@ void ULN2003APG::one_phase(int pulse_frequency)
     _IN3 = 1;
     ThisThread::sleep_for(chrono::milliseconds(v));
     _IN3 = 0;
+
+    _IN4 = 1;
+    ThisThread::sleep_for(chrono::milliseconds(v));
+    _IN4 = 0;
+}
+
+void ULN2003APG::two_phase(int pulse_frequency)
+{
+    int v = hz_to_ms(pulse_frequency);
+
+    _IN1 = 1;
+    _IN2 = 1;
+    ThisThread::sleep_for(chrono::milliseconds(v));
+    _IN1 = 0;
+    _IN2 = 0;
+
+    _IN2 = 1;
+    _IN3 = 1;
+    ThisThread::sleep_for(chrono::milliseconds(v));
+    _IN2 = 0;
+    _IN3 = 0;
+
+    _IN3 = 1;
+    _IN4 = 1;
+    ThisThread::sleep_for(chrono::milliseconds(v));
+    _IN3 = 0;
+    _IN4 = 0;
+
+    _IN4 = 1;
+    _IN1 = 1;
+    ThisThread::sleep_for(chrono::milliseconds(v));
+    _IN4 = 0;
+    _IN1 = 0;
+}
+
+void ULN2003APG::half_step(int pulse_frequency)
+{
+    int v = hz_to_ms(pulse_frequency);
+    
+    _IN1 = 1;
+    ThisThread::sleep_for(chrono::milliseconds(v));
+    _IN1 = 0;
+
+    _IN1 = 1;
+    _IN2 = 1;
+    ThisThread::sleep_for(chrono::milliseconds(v));
+    _IN1 = 0;
+    _IN2 = 0;
+
+    _IN2 = 1;
+    ThisThread::sleep_for(chrono::milliseconds(v));
+    _IN2 = 0;
+
+    _IN2 = 1;
+    _IN3 = 1;
+    ThisThread::sleep_for(chrono::milliseconds(v));
+    _IN2 = 0;
+    _IN3 = 0;
+
+    _IN3 = 1;
+    ThisThread::sleep_for(chrono::milliseconds(v));
+    _IN3 = 0;
+
+    _IN3 = 1;
+    _IN4 = 1;
+    ThisThread::sleep_for(chrono::milliseconds(v));
+    _IN3 = 0;
+    _IN4 = 0;
+
+    _IN4 = 1;
+    ThisThread::sleep_for(chrono::milliseconds(v));
+    _IN4 = 0;
+
+    _IN4 = 1;
+    _IN1 = 1;
+    ThisThread::sleep_for(chrono::milliseconds(v));
+    _IN4 = 0;
+    _IN1 = 0;
+}
+
+// private
+int ULN2003APG::hz_to_ms(int frequency)
+{
+    return (int)(1000.0 / frequency);
 }
--- ULN2003APG.h
+++ ULN2003APG.h
@@ -6,12 +6,36 @@
 class ULN2003APG
 {
 public:
+    /**
+     * @brief コンストラクタです。ULN2003APGのIN1~IN4に接続されたピンを指定してください。
+     * @param IN1 ULN2003APGのIN1に接続されたピン(DigitalOut)
+     * @param IN2 ULN2003APGのIN2に接続されたピン(DigitalOut)
+     * @param IN3 ULN2003APGのIN3に接続されたピン(DigitalOut)
+     * @param IN4 ULN2003APGのIN4に接続されたピン(DigitalOut)
+     */
     ULN2003APG(PinName IN1, PinName IN2, PinName IN3, PinName IN4);
 
+    /**
+     * @brief ステッピングモーターを停止します。
+     */
+    void step_stop();
+
+    /**
+     * @brief 1相励磁します。
+     * @param pulse_frequency パルス周波数
+     */
     void one_phase(int pulse_frequency);
     
+    /**
+     * @brief 2相励磁します。
+     * @param pulse_frequency パルス周波数
+     */
     void two_phase(int pulse_frequency);
     
+    /**
+     * @brief ハーフステップ励磁します。
+     * @param pulse_frequency パルス周波数
+     */
     void half_step(int pulse_frequency);
 
 private:
@@ -20,8 +44,11 @@ private:
     DigitalOut _IN3;
     DigitalOut _IN4;
 
-    DigitalOut _step_in[4];
-
+    /**
+     * @brief 周波数をミリ秒に変換します。
+     * @param frequency 周波数
+     * @return ミリ秒
+     */
     int hz_to_ms(int frequency);
 };
 
